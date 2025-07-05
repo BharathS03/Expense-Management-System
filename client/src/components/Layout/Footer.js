61234257ae30dee52f93c73337a3bbc525a1aba3
BharathS03 <bharathsunder107@gmail.com>
2025-07-05 09:31:54 +0530

    commit
    commit expense management system

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; techinfoyt
+      </h6>
+    </div>
+
+  );
+};
+
+export default Footer;
